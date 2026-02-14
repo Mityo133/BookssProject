198d7b5303348c556a5421b519bf78bd6239e0da
Mityo133 <dpolugchiew@gmail.com>
2026-02-14 11:18:36 +0200

    sa

---
+++ Bookss/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './Views/**/*.cshtml',
+    './wwwroot/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
